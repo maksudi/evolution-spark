35792c0a987eefcc1e53c2c419620f2147795464
maksudi <maksud@ibragimov.org>
2016-03-21 00:28:25 +1100

    - Some ideas on functional types

--- src/main/scala/au/com/adaptic/launcher/Main.scala
+++ src/main/scala/au/com/adaptic/launcher/Main.scala
@@ -6,5 +6,21 @@ package main.scala
 
 
 object Main {
-  println("hello, evolution")
+  type Data = List[Int]
+  type AlgorithmResult = Double
+  type Solution = Double
+  type EvalFunction = Solution => Double
+  type Algorithm = Data => EvalFunction => AlgorithmResult
+  
+  
+  class Individual{
+  }
+  
+//  class SimpleIndividual extends List[Int] {
+//    
+//  }
+  
+  type Population = Seq[Individual]
+  type SelectionPool = List[Individual]
+  type Selection = Population => SelectionPool //need type safety for Selection pool here
 }
